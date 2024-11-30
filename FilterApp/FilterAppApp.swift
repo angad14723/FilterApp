e64c9ed2e22630c9d15c7a6502455bd1e439bea9
Rahul Singh <angad14723@gmail.com>
2024-11-30 17:58:48 +0530

    Initial Commit

---
+++ FilterApp/FilterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilterAppApp.swift
+//  FilterApp
+//
+//  Created by Angad on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct FilterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
